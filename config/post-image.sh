a0fb07989f390cf9192eb346b4797d3727dcb792
bogics <bogics@gmail.com>
2015-10-17 13:54:56 +0200

    Add root password to root user. Add user pi with password rpi

--- config/post-image.sh
+++ config/post-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 sudo rm -rf "$BINARIES_DIR/rootfs"
 mkdir "$BINARIES_DIR/rootfs"
 sudo tar xf "$BINARIES_DIR/rootfs.tar" -C "$BINARIES_DIR/rootfs"
-#sudo chown -R $USER:$USER "$BINARIES_DIR/rootfs"
+
+sudo chown 9374:9374 "$BINARIES_DIR/rootfs/home/pi"
